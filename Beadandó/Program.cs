439d277a1d52f38c033fabecb7b6612e5df4339a
Fábián Gábor <info@fabiangabor.com>
2021-02-23 20:39:36 +0100

    Pi kiszámolása Gregory-Leibniz sorozattal

--- Beadandó/Program.cs
+++ Beadandó/Program.cs
@@ -30,34 +30,44 @@ namespace Beadandó
         XII = 12
     }
 
-    class Pont
+    internal class Pont
     {
-        private double x;
-        private double y;
-
         public Pont(double x = default, double y = default)
         {
-            this.x = x;
-            this.y = y;
+            X = x;
+            Y = y;
         }
 
-        public double X
-        {
-            get => x;
-            set => x = value;
-        }
+        public double X { get; set; }
 
-        public double Y
-        {
-            get => y;
-            set => y = value;
-        }
+        public double Y { get; set; }
 
         // Készíts void függvényt, ami a sík egy derékszögű koordinátarendszerben adott pontját tükrözi a függőleges tengely mentén!
         public static void KoordinataTukrozFuggolegesTengely(in Pont p, out Pont pont)
         {
             pont = p;
-            pont.x *= -1;
+            pont.X *= -1;
+        }
+    }
+
+    /*
+    3. Készíts függvényt, ami a paraméterében megadott iterációszámmal közelítőleg visszaadja a PI értékét! Használjuk a Gregory-Leibniz sorozatot:
+    ? = (4/1) - (4/3) + (4/5) - (4/7) + (4/9) - (4/11) + (4/13) - (4/15) + ...
+    */
+    internal class Pi
+    {
+        private double Value { get; set; }
+
+        public double Calculate(in int n)
+        {
+            double value = 0;
+            for (var i = 0; i < n; i++)
+            {
+                // Value += Math.Pow(-1, i) * (4.0 / (2 * i + 1)); // 2.52% get 162ms + 1.77% set 114 ms
+                value += Math.Pow(-1, i) * (4.0 / (2 * i + 1));
+            }
+
+            return value;
         }
     }
 
@@ -69,7 +79,7 @@ namespace Beadandó
          */
         private static readonly Dictionary<int, string> ArabRomai = new Dictionary<int, string>
         {
-            {10,"X"},
+            {10, "X"},
             {9, "IX"},
             {5, "V"},
             {4, "IV"},
@@ -77,23 +87,22 @@ namespace Beadandó
         };
 
         private static string ArabRomaiKoverzio(int arabSzam)
-        { 
+        {
             var romaiSzam = string.Empty;
-	
+
             foreach (var arabErtek in ArabRomai.Keys)
-            {
                 while (arabSzam >= arabErtek)
                 {
                     romaiSzam += ArabRomai[arabErtek];
                     arabSzam -= arabErtek;
                 }
-            }
+
             return romaiSzam;
         }
-        
+
         public static void Main(string[] args)
         {
-            DateTime date = DateTime.Today;
+            //var date = DateTime.Today;
             /*
             for (int i = 1; i <= 12; i++)
             {
@@ -106,14 +115,18 @@ namespace Beadandó
                 Console.WriteLine(i + " " + ArabRomaiKoverzio(i));
             }
             */
+            /*
             Console.WriteLine("Mai datum: " + date.Year + "." + ArabRomaiKoverzio(date.Month) + "." + date.Day + ".");
-            Console.WriteLine("Mai datum: " + date.Year + "." + Enum.Parse(typeof(RomaiSzamok), date.Month.ToString()) + "." + date.Day + ".");
+            Console.WriteLine("Mai datum: " + date.Year + "." + Enum.Parse(typeof(RomaiSzamok), date.Month.ToString()) +
+                              "." + date.Day + ".");
 
-            Pont p = new Pont(1,1);
+            var p = new Pont(1, 1);
             Console.WriteLine("(" + p.X + "," + p.Y + ")");
             Pont.KoordinataTukrozFuggolegesTengely(in p, out p);
             Console.WriteLine("(" + p.X + "," + p.Y + ")");
-
+            */
+            var pi = new Pi();
+            Console.WriteLine(pi.Calculate(100000000));
         }
     }
 }
